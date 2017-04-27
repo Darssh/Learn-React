be7a95c74c5bd9372b615e9a9c0d86974904d84d
Darssh <darshitsuthar91@gmail.com>
2017-04-27 10:18:46 -0700

    Learn_react

---
+++ react-learn/src/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function Welcome(props) {
+	return <h1>Hello, {props.name}</h1>;
+}
+
+function formatDate(date) {
+	return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props) {
+	return (
+		<div className="Comment">
+			<UserInfo user={props.author} />
+			<div className="Comment-text">
+				{props.text}
+			</div>
+			<div className="Comment-date">
+				{formatDate(props.date)}
+			</div>
+		</div>
+	);
+}
+
+const comment = {
+	date: new Date(),
+	text: 'I hope you enjoy!',
+	author: {
+		name: 'Hello',
+		avatarUrl: 'http://placekitten.com/g/64/64'
+	}
+}
+
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {date: new Date()};
+	}
+
+	componentDidMount() {
+		
+	}
+
+	componentWillUnmount() {
+		
+	}
+	
+	render() {
+		return (
+			<div>
+				<h1>Hello, world!</h1>
+				<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<Clock />,
+    document.getElementById('root')
+);
