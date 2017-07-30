5070c27077d19d2b2b6c1392fe33f07d795f7cec
Darssh <darshitsuthar91@gmail.com>
2017-07-30 12:55:03 -0700

    Post method in signup and login page.

--- React-Auth/client/src/containers/LoginPage.jsx
+++ React-Auth/client/src/containers/LoginPage.jsx
@@ -21,8 +21,30 @@ class LoginPage extends React.Component {
     processForm(event) {
         event.preventDefault();
 
-        console.log('email:', this.state.user.email);
-        console.log('password:', this.state.user.password);
+        const email = encodeURIComponent(this.state.user.email);
+        const password = encodeURIComponent(this.state.user.password);
+        const formData = `email=${email}&password=${password}`;
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('post', '/auth/login');
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.responseType = 'json';
+        xhr.addEventListener('load', () => {
+            if (xhr.status === 200) {
+                this.setState({
+                    errors: {}
+                });
+                console.log('The form is valid');
+            } else {
+                const errors = xhr.response.errors ? xhr.response.errors : {};
+                errors.summary = xhr.response.message;
+
+                this.setState({
+                    errors
+                });
+            }
+        });
+        xhr.send(formData);
     }
 
     changeUser(event) {
